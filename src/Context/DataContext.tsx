10949d14dc5acd5ba56d3830bfec8cd91b7ab532
Democrito <demohenry6@gmail.com>
2024-02-06 13:59:49 -0300

    Implementa função especifica para extrair data de hoje e aplicar ao estado de inicio e final

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -1,10 +1,20 @@
-import React, { PropsWithChildren, useContext } from 'react';
+import React, {
+	Dispatch,
+	PropsWithChildren,
+	SetStateAction,
+	useContext,
+	useState,
+} from 'react';
 import useFetch from '../Hooks/useFetch';
 
 type IDataContext = {
 	data: IVenda[] | null;
 	loading: boolean;
 	error: string | null;
+	inicio: string;
+	final: string;
+	setInicio: Dispatch<SetStateAction<string>>;
+	setFinal: Dispatch<SetStateAction<string>>;
 };
 
 type IVenda = {
@@ -25,13 +35,26 @@ export const useData = () => {
 	return context;
 };
 
+function getDaysAgo(n: number) {
+	const date = new Date();
+	date.setDate(date.getDate() - n);
+
+	const dateString = date.toISOString().split('T')[0];
+	return dateString;
+}
+
 export const DataContextProvider = ({ children }: PropsWithChildren) => {
+	const [inicio, setInicio] = React.useState(getDaysAgo(20));
+	const [final, setFinal] = useState(getDaysAgo(0));
+
 	const { data, loading, error } = useFetch<IVenda[]>(
-		'https://data.origamid.dev/vendas/'
+		`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
 	);
 
 	return (
-		<DataContext.Provider value={{ data, loading, error }}>
+		<DataContext.Provider
+			value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+		>
 			{children}
 		</DataContext.Provider>
 	);
