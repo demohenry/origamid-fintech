153eeba70a66642441e1a8bedd85fdef5f12a506
Democrito <demohenry6@gmail.com>
2024-02-08 11:43:16 -0300

    Estruturação e logica de funcionamento de componente de Resumo

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -4,9 +4,57 @@ import { useData } from '../Context/DataContext';
 const Resumo = () => {
 	const { data } = useData();
 
-	console.log(data);
-
-	return <div>Resumo</div>;
+	if (!data) return null;
+	return (
+		<section>
+			<div className='resumo flex mb'>
+				<div className='box'>
+					<h2>Vendas</h2>
+					<span>
+						{data
+							.filter((i) => i.status !== 'falha')
+							.reduce((acc, vendas) => acc + vendas.preco, 0)
+							.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+					</span>
+					<div>
+						<span
+							style={{ color: 'red', fontSize: '10px', whiteSpace: 'noWrap' }}
+							title='Vendas que não tiveram o status concluído'
+						>
+							{data
+								.filter((i) => i.status == 'falha')
+								.reduce((acc, vendas) => acc + vendas.preco, 0)
+								.toLocaleString('pt-BR', {
+									style: 'currency',
+									currency: 'BRL',
+								})}{' '}
+							(Transações falhas -{' '}
+							{data.filter((i) => i.status == 'falha').length})
+						</span>
+					</div>
+				</div>
+				<div className='box'>
+					<h2>Recebido</h2>
+					<span>
+						{data
+							.filter((i) => i.status == 'pago')
+							.reduce((acc, vendas) => acc + vendas.preco, 0)
+							.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+					</span>
+				</div>
+				<div className='box'>
+					<h2>Processando</h2>
+					<span>
+						{data
+							.filter((i) => i.status == 'processando')
+							.reduce((acc, vendas) => acc + vendas.preco, 0)
+							.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+					</span>
+				</div>
+			</div>
+			<div className='box'>Gráficos</div>
+		</section>
+	);
 };
 
 export default Resumo;
