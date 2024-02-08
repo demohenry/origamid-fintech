254a565874e93aef8c2d2c81ed3f8e9d328a19be
Democrito <demohenry6@gmail.com>
2024-02-08 17:59:08 -0300

    Adiciona principais elementos classes e para o funcionamento do componente de SVGComponent juntamente com os primeiros passos do Sidenav

--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import fintech from '../assets/fintech.svg';
+import resumo from '../assets/icons/resumo.svg';
+import vendas from '../assets/icons/vendas.svg';
+import SVGComponent, { SVGLibrary } from '../assets/SVGComponent';
 
 const Sidenav = () => {
 	return (
-		<div>
-			<h3>Sidenav</h3>
-		</div>
+		<nav className='sidenav box bg-3'>
+			<SVGComponent children={SVGLibrary.fintech} height='50' />
+			<ul>
+				<li>
+					<span>
+						<img src={resumo} alt='' />
+					</span>
+					<a href=''>Resumo</a>
+				</li>
+				<li>
+					<span>
+						<img src={vendas} alt='' />
+					</span>
+					<a href=''>Vendas</a>
+				</li>
+			</ul>
+		</nav>
 	);
 };
 
