827d1c4c5965d2ce5b362c61bf2c0f850b001e47
Democrito <demohenry6@gmail.com>
2024-02-08 18:31:04 -0300

    Novas classes para daterange demostrando pagina atual aberta

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useData } from '../Context/DataContext';
+import { useState } from 'react';
 import DateRange from './DateRange';
 import Month from './Months';
 
 const Header = () => {
+	const [title, setTitle] = useState('Resumo');
+
 	return (
 		<header className='mb'>
-			<div className='mb'>
+			<div className='daterange mb'>
 				<DateRange />
+				<h1 className='box bg-3'>{title}</h1>
 			</div>
 			<Month />
 		</header>
